1d010e2324317c37cb4fadfab4bb51e19b8a1532
BearBB <cc861010@gmail.com>
2014-02-17 22:34:24 +0800

    myrtc

---
+++ resource/MyRTC.js
@@ -0,0 +1,73 @@
+//http://log.medcl.net/item/2013/05/1104/
+/**
+First of all; create "offer sdp" by calling peerConnection.createOffer:
+
+offerer.createOffer(function (offerSDP) {
+    offerer.setLocalDescription(offerSDP);
+    // use XHR/WebSocket/etc. to exchange offer-sdp with other peer(s)
+}, null, constraints);
+
+On the "answerer" side, set "remote descriptions" using "offer sdp":
+
+offerSDP = new SessionDescription(offerSDP);
+answerer.setRemoteDescription(offerSDP);
+And then create "answer sdp":
+answerer.createAnswer(function (answerSDP) {
+    answerer.setLocalDescription(answerSDP);
+
+    // use XHR/WebSocket/etc. to exchange answer-sdp with "offerer"
+}, null, constraints);
+
+On the offerer side, set remote descriptions using "answer sdp":
+
+answerSDP = new SessionDescription(answerSDP);
+offerer.setRemoteDescription(answerSDP);
+
+ **/
+var iceServers = {
+    iceServers:[
+        {url: 'stun:stun.l.google.com:19302'}, //STUN
+        {url: 'turn:homeo@turn.bistri.com:80',credential: 'homeo'}  //TURN
+    ]
+};
+
+var optional = {
+    optional: [{
+        RtpDataChannels: true
+    }]
+};
+//////////////////////////////////////////////////////////////////////////
+//first step
+//////////////////////////////////////////////////////////////////////////
+var peer = new webkitRTCPeerConnection(iceServers, optional);
+var constraints;
+var sdp;  //RTCSessionDescription object
+peer.createOffer(
+    function(sessionDescription) {
+        peer.setLocalDescription(sessionDescription);
+        sdp = sessionDescription;
+        // use XHR/WebSocket/etc. to exchange offer-sdp with other peer(s)
+    },
+    function onSdpError(e) {
+        console.error('sdp error:', e.name, e.message);
+    },
+    constraints
+);
+//////////////////////////////////////////////////////////////////////////
+//second step
+//////////////////////////////////////////////////////////////////////////
+
+var answer;
+var offerSDP = new RTCSessionDescription({sdp:sdp.sdp,type:sdp.type});
+var otherPeer = new webkitRTCPeerConnection(iceServers, optional);
+otherPeer.setRemoteDescription(offerSDP);
+otherPeer.createAnswer(function (answerSDP) {
+    otherPeer.setLocalDescription(answerSDP);
+    answer = answerSDP;
+    // use XHR/WebSocket/etc. to exchange answer-sdp with "offerer"
+}, null, constraints);
+//////////////////////////////////////////////////////////////////////////
+//third step
+//////////////////////////////////////////////////////////////////////////
+answerSDP = new SessionDescription({sdp:answer.sdp,type:answer.type});
+offerer.setRemoteDescription(answerSDP);
