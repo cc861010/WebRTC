a7f7ebb4eb0e4b186a3a98276428447ac0563f15
cc <cc861010@gmail.com>
2014-03-13 10:19:01 +0800

    rtcdatachannel

--- resource/myotherrtc.js
+++ resource/myotherrtc.js
@@ -1,14 +1,5 @@
 var sendChannel, receiveChannel;
 
-var startButton = document.getElementById("startButton");
-var sendButton = document.getElementById("sendButton");
-var closeButton = document.getElementById("closeButton");
-startButton.disabled = false;
-sendButton.disabled = true;
-closeButton.disabled = true;
-startButton.onclick = createConnection;
-sendButton.onclick = sendData;
-closeButton.onclick = closeDataChannels;
 
 function trace(text) {
     console.log((performance.now() / 1000).toFixed(3) + ": " + text);
@@ -16,20 +7,53 @@ function trace(text) {
 
 function createConnection() {
     var servers = null;
+    /*
+     * setup localPeerConnection
+     */
     window.localPeerConnection = new webkitRTCPeerConnection(servers,{optional: [{RtpDataChannels: true}]});
     trace('Created local peer connection object localPeerConnection');
     sendChannel = localPeerConnection.createDataChannel("sendDataChannel",{reliable: false});
     trace('Created send data channel');
-    localPeerConnection.onicecandidate = gotLocalCandidate;
-    sendChannel.onopen = handleSendChannelStateChange;
-    sendChannel.onclose = handleSendChannelStateChange;
-
+    localPeerConnection.onicecandidate = function(event) {
+        trace('local ice callback');
+        if (event.candidate) {
+            remotePeerConnection.addIceCandidate(event.candidate);
+            trace('Local ICE candidate: \n' + event.candidate.candidate);
+        }
+    };
+    sendChannel.onopen = function(event){trace('sendChannel:onopen');};;
+    sendChannel.onclose = function(event){trace('sendChannel:onclose');};;
+    /*
+     * setup remotePeerConnection
+     */
     window.remotePeerConnection = new webkitRTCPeerConnection(servers,{optional: [{RtpDataChannels: true}]});
     trace('Created remote peer connection object remotePeerConnection');
-    remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
-    remotePeerConnection.ondatachannel = gotReceiveChannel;
+    remotePeerConnection.onicecandidate = function(event) {
+        trace('remote ice callback');
+        if (event.candidate) {
+            localPeerConnection.addIceCandidate(event.candidate);
+            trace('Remote ICE candidate: \n ' + event.candidate.candidate);
+        }
+    };
+    remotePeerConnection.ondatachannel = function(event) {
+        trace('Receive Channel Callback');
+        receiveChannel = event.channel;
+        receiveChannel.onmessage = function(event){trace('gotReceiveChannel:onmessage->'+event.data);};
+        receiveChannel.onopen = function(){trace('gotReceiveChannel:onopen');}
+        receiveChannel.onclose = function(){trace('gotReceiveChannel:onclose');}
+        receiveChannel.send = function(data){receiveChannel.send(data);}
+    };
 
-    localPeerConnection.createOffer(gotLocalDescription);
+    localPeerConnection.createOffer(function(desc) {
+        localPeerConnection.setLocalDescription(desc);
+        trace('Offer from localPeerConnection \n' + desc.sdp);
+        remotePeerConnection.setRemoteDescription(desc);
+        remotePeerConnection.createAnswer(function(desc) {
+            remotePeerConnection.setLocalDescription(desc);
+            trace('Answer from remotePeerConnection \n' + desc.sdp);
+            localPeerConnection.setRemoteDescription(desc);
+        });
+    });
 
 }
 
@@ -46,45 +70,8 @@ function closeDataChannels() {
     trace('Closed data channel with label: ' + receiveChannel.label);
     localPeerConnection.close();
     remotePeerConnection.close();
-    localPeerConnection = null;
-    remotePeerConnection = null;
     trace('Closed peer connections');
 }
 
-function gotLocalDescription(desc) {
-    localPeerConnection.setLocalDescription(desc);
-    trace('Offer from localPeerConnection \n' + desc.sdp);
-    remotePeerConnection.setRemoteDescription(desc);
-    remotePeerConnection.createAnswer(gotRemoteDescription);
-}
 
-function gotRemoteDescription(desc) {
-    remotePeerConnection.setLocalDescription(desc);
-    trace('Answer from remotePeerConnection \n' + desc.sdp);
-    localPeerConnection.setRemoteDescription(desc);
-}
-
-function gotLocalCandidate(event) {
-    trace('local ice callback');
-    if (event.candidate) {
-        remotePeerConnection.addIceCandidate(event.candidate);
-        trace('Local ICE candidate: \n' + event.candidate.candidate);
-    }
-}
-
-function gotRemoteIceCandidate(event) {
-    trace('remote ice callback');
-    if (event.candidate) {
-        localPeerConnection.addIceCandidate(event.candidate);
-        trace('Remote ICE candidate: \n ' + event.candidate.candidate);
-    }
-}
-
-function gotReceiveChannel(event) {
-    trace('Receive Channel Callback');
-    receiveChannel = event.channel;
-    receiveChannel.onmessage = function(event){trace('Received message: ' + event.data);};
-    receiveChannel.onopen = function(){trace('Send channel state is: ' + sendChannel.readyState);};
-    receiveChannel.onclose = function(){trace('Send channel state is: ' + sendChannel.readyState);};
-}
 
