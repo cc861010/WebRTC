3803262d2d3f5d0abc5877eb6eb6f46ef313ab43
cc <cc861010@gmail.com>
2014-03-16 22:42:23 +0800

    newrtc

--- resource/fireRTC/NewRTC.js
+++ resource/fireRTC/NewRTC.js
@@ -1,4 +1,3 @@
-var pc1, pc2, offer, answer;
 
 pc1 = new webkitRTCPeerConnection(null,{optional: [{RtpDataChannels: true}]});
 sendChannel = pc1.createDataChannel("sendDataChannel",{reliable: true});
@@ -6,10 +5,15 @@ sendChannel.onopen = function(event){console.log('sendChannel:onopen');};
 sendChannel.onclose = function(event){console.log('sendChannel:onclose');};
 sendChannel.onmessage = function(event){console.log('sendChannel:onmessage->'+event.data);};
 signaling1.onMessage("addIceCandidate",function(e){
-    pc1.addIceCandidate(e);
+    var message = JSON.parse(e)
+    if(message.candidate && message.candidate!=null){
+        pc1.addIceCandidate(new RTCIceCandidate(message.candidate));
+        console.log("pc1.addIceCandidate");
+    }
 })
 signaling1.onMessage("setRemoteDescription",function(e){
-    pc1.setRemoteDescription(e, onPc1RemoteDescriptionSet, onError);
+    pc1.setRemoteDescription(new RTCSessionDescription(JSON.parse(e)), onPc1RemoteDescriptionSet, onError);
+    console.log("pc1.setRemoteDescription");
 });
 
 
@@ -23,10 +27,15 @@ pc2.ondatachannel = function(event) {
     receiveChannel.onmessage = function(event){console.log('gotReceiveChannel:onmessage->'+event.data);};
 };
 signaling2.onMessage("addIceCandidate",function(e){
-    pc2.addIceCandidate(e);
+    var message = JSON.parse(e)
+    if(message.candidate && message.candidate!=null){
+        pc2.addIceCandidate(new RTCIceCandidate(message.candidate));
+        console.log("pc1.addIceCandidate");
+    }
 })
 signaling2.onMessage("setRemoteDescription",function(e){
-    pc2.setRemoteDescription(e, onPc2RemoteDescriptionSet, onError);
+   pc2.setRemoteDescription(new RTCSessionDescription(JSON.parse(e)), onPc2RemoteDescriptionSet, onError);
+   console.log("pc2.setRemoteDescription");
 });
 
 
@@ -35,29 +44,29 @@ signaling2.onMessage("setRemoteDescription",function(e){
 pc1.onicecandidate = function(event) {
     if (event.candidate){
         //pc2.addIceCandidate(event.candidate);
-        signaling2.emitMessage("addIceCandidate",event.candidate);
-        console.log("pc2.addIceCandidate"+ event.candidate.candidate)
+        signaling2.emitMessage("addIceCandidate",JSON.stringify(event.candidate));
+        //console.log("pc2.addIceCandidate"+ event.candidate.candidate)
     }
 };
 
 pc2.onicecandidate = function(event) {
     if (event.candidate){
         //pc1.addIceCandidate(event.candidate);
-        signaling1.emitMessage("addIceCandidate",event.candidate);
-        console.log("pc1.addIceCandidate"+ event.candidate.candidate)}
+        signaling1.emitMessage("addIceCandidate",JSON.stringify(event.candidate));
+        //console.log("pc1.addIceCandidate"+ event.candidate.candidate)
+    }
 };
 
 
 
 function onOfferCreated(description) {
-    offer = description;
-    pc1.setLocalDescription(offer, onPc1LocalDescriptionSet, onError);
+    pc1.setLocalDescription(description, onPc1LocalDescriptionSet(description), onError);
 }
 /////////////////////////////////////////////////////////////////////////////////////////
-function onPc1LocalDescriptionSet() {
+function onPc1LocalDescriptionSet(offer) {
     // after this function returns, pc1 will start firing icecandidate events
     //pc2.setRemoteDescription(offer, onPc2RemoteDescriptionSet, onError);
-    signaling1.emitMessage("setRemoteDescription",offer);
+    signaling1.emitMessage("setRemoteDescription",JSON.stringify(offer));
 }
 
 function onPc2RemoteDescriptionSet() {
@@ -65,14 +74,13 @@ function onPc2RemoteDescriptionSet() {
 }
 
 function onAnswerCreated(description) {
-    answer = description;
-    pc2.setLocalDescription(answer, onPc2LocalDescriptionSet, onError);
+    pc2.setLocalDescription(description, onPc2LocalDescriptionSet(description), onError);
 }
 /////////////////////////////////////////////////////////////////////////////////////////
-function onPc2LocalDescriptionSet() {
+function onPc2LocalDescriptionSet(answer) {
     // after this function returns, you'll start getting icecandidate events on pc2
     //pc1.setRemoteDescription(answer, onPc1RemoteDescriptionSet, onError);
-    signaling2.emitMessage("setRemoteDescription",answer);
+    signaling2.emitMessage("setRemoteDescription",JSON.stringify(answer));
 }
 
 
